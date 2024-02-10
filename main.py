562fb3680c3664b7e1e9780eba496defa4aaffcf
mgonzalz <mgonzgarc@myuax.com>
2024-02-10 20:58:38 +0100

    objetos

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ root.iconbitmap('img/roomba.ico')
 title = Label(root, text='Roomba Simulator: Habitaciones rectangulares/cuadradas', font=('Modern', 20, 'bold'))
 title.grid(row=0, column=0, columnspan=2, pady=20, sticky='nsew')
 
-# INFORMACIÓN
-frame = Frame(root, width=30)
+# INFORMACIÓN: Habitación
+frame = Frame(root, width=20)
 frame.grid(row=1, column=0, sticky='w')
 
     # HABITACIÓN
@@ -19,9 +19,8 @@ nombre = StringVar()
 x = StringVar()
 y = StringVar()
 
-Label(frame, text='Nombre de la habitación: ').grid(row=0, column=0, padx=10, pady=10, sticky='w')
+Label(frame, text='Nombre de la habitación: ', width=20).grid(row=0, column=0, padx=10, pady=10, sticky='w')
 Entry(frame, justify='right', width=15, textvariable=nombre).grid(row=0, column=1)
-Label(frame, text='m').grid(row=0, column=2, sticky='w')
 
 Label(frame, text='Ancho de la habitación: ').grid(row=1, column=0, padx=10, pady=10, sticky='w')
 Entry(frame, justify='right', width=15, textvariable=x).grid(row=1, column=1)
@@ -34,6 +33,38 @@ Label(frame, text='m').grid(row=2, column=2, sticky='w')
 button_hab = Button(frame, text='Agregar habitación', bg='#B7AEA1', fg='white', border=0, width=4, command=lambda:add_habitacion(nombre, x, y))
 button_hab.grid(row=3, column=0, columnspan=2, padx=15, pady=10, sticky='nsew')
 
+# INFORMACIÓN: Objeto
+frame2 = Frame(root, width=20)
+frame2.grid(row=2, column=0, sticky='w')
     # OBJETO
+hab_obj = StringVar()
+nombre_obj= StringVar()
+x_obj = StringVar()
+y_obj = StringVar()
+coord_x = StringVar()
+coord_y = StringVar()
+
+Label(frame2, text='Habitación: ', width=20, anchor='w').grid(row=0, column=0, padx= 10, pady=10)
+Entry(frame2, justify='right', width=15, textvariable=hab_obj).grid(row=0, column=1)
+
+Label(frame2, text='Nombre del objeto: ').grid(row=1, column=0, padx=10, pady=10, sticky='w')
+Entry(frame2, justify='right', width=15, textvariable=nombre_obj).grid(row=1, column=1)
+
+Label(frame2, text='Ancho del objeto: ').grid(row=2, column=0, padx=10, pady=10, sticky='w')
+Entry(frame2, justify='right', width=15, textvariable=x_obj).grid(row=2, column=1)
+Label(frame2, text='m').grid(row=2, column=2, sticky='w')
+
+Label(frame2, text='Alto del objeto: ').grid(row=3, column=0, padx=10, pady=10, sticky='w')
+Entry(frame2, justify='right', width=15, textvariable=y_obj).grid(row=3, column=1)
+Label(frame2, text='m').grid(row=3, column=2, sticky='w')
+
+Label(frame2, text='Coordenada X: ').grid(row=4, column=0, padx=10, pady=10, sticky='w')
+Entry(frame2, justify='right', width=15, textvariable=coord_x).grid(row=4, column=1)
+
+Label(frame2, text='Coordenada Y: ').grid(row=5, column=0, padx=10, pady=10, sticky='w')
+Entry(frame2, justify='right', width=15, textvariable=coord_y).grid(row=5, column=1)
+
+button_obj = Button(frame2, text='Agregar objeto', bg='#B7AEA1', fg='white', border=0, width=4, command=lambda:add_object(hab_obj, nombre_obj, x_obj, y_obj, coord_x, coord_y))
+button_obj.grid(row=6, column=0, columnspan=2, padx=15, pady=10, sticky='nsew')
 
 root.mainloop()
--- modulos/utils.py
+++ modulos/utils.py
@@ -11,13 +11,47 @@ def add_habitacion(nombre_hab, x_hab, y_hab):
             x = float(x)
             y = float(y)
             if x > 0 and y > 0:
-
                 habitacion = Zona(nombre, x, y)
                 habitaciones.append(habitacion)
                 for i in [x_hab, y_hab , nombre_hab]:
                     i.set('')
                 messagebox.showinfo('Agregado', f'Habitación {nombre} agregada con éxito.', icon=messagebox.INFO)
+            else:
+                messagebox.showerror('Error', 'Los valores deben ser positivos.')
         except ValueError:
-            messagebox.showerror('Error', 'Los valores deben ser numéricos positivos.')
+            messagebox.showerror('Error', 'Los valores deben ser numéricos.')
     else:
         messagebox.showerror('Error', 'Todos los campos son obligatorios.')
+
+
+def add_object(hab_var, nombre_var, x_var, y_var, coord_x_var, coord_y_var):
+    hab = hab_var.get()
+    nombre = nombre_var.get()
+    x = x_var.get()
+    y = y_var.get()
+    coord_x = coord_x_var.get()
+    coord_y = coord_y_var.get()
+
+    if hab and nombre and x and y and coord_x and coord_y:
+        try:
+            x = float(x)
+            y = float(y)
+            coord_x = float(coord_x)
+            coord_y = float(coord_y)
+
+            if x > 0 and y > 0:
+                for habitacion in habitaciones:
+                    if habitacion.nombre == hab:
+                        habitacion.add_objeto(nombre, x, y, coord_x, coord_y)
+
+                        for var in [hab_var, nombre_var, x_var, y_var, coord_x_var, coord_y_var]:
+                            var.set('')
+
+                        messagebox.showinfo('Agregado', f'Objeto {nombre} agregado con éxito en la habitación {hab}.', icon=messagebox.INFO)
+                        break
+                else:
+                    messagebox.showerror('Error', f'La habitación {hab} no existe.', icon=messagebox.ERROR)
+        except ValueError:
+            messagebox.showerror('Error', 'Los valores deben ser numéricos positivos.', icon=messagebox.ERROR)
+    else:
+        messagebox.showerror('Error', 'Todos los campos son obligatorios.', icon=messagebox.ERROR)
